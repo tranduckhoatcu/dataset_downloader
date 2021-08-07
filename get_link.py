66515ebf8dc08f5e938f84fe131bcf4b9d6c8f48
Khoa Tran <tranduckhoatcu>
2021-08-08 01:59:59 +0700

    test

--- get_link.py
+++ get_link.py
@@ -33,7 +33,7 @@ class WebDriverChrome(object):
 
 
         scroll_counter = 0
-        for i in range(30):
+        for i in range(1):
             print(f'{scroll_counter} scroll times')
             scroll_counter += 1
             self.driver.execute_script("window.scrollBy(0,"+str(secrets.SystemRandom().uniform(1800,2000))+");")
